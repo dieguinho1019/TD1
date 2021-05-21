90f76545cb5a2a2c77f526f1deef214a9dcc1285
Diego <diegoeldieguinho@gmail.com>
2021-05-21 12:39:09 -0500

    14

--- api.php
+++ api.php
@@ -12,7 +12,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } else {
-    echo "Connected successfully2";
 
     $sql = "SELECT * FROM dbSigfox.tblMensajes";
     $result = $conn->query($sql);
