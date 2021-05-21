5fcb7551776e8373dcf7425868c98870f65d72a9
Diego <diegoeldieguinho@gmail.com>
2021-05-21 12:28:28 -0500

    11

--- api.php
+++ api.php
@@ -1,20 +1,30 @@
 <?php
-$mysqli = new mysqli("localhost","diego","TesisDiego123","dbSigfox");
 
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
+$servername = "localhost";
+$username = "diego";
+$password = "TesisDiego123";
+$dbname = "dbSigfox";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully2";
+
+    $sql = "SELECT * FROM dbSigfox.tblMensajes";
+    $result = $conn->query($sql);
+
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        }
+      } else {
+        echo "0 results";
+      }
+      $conn->close();
 }
-
-$sql = "SELECT * FROM tblMensajes";
-$result -> $mysqli -> query($sql);
-
-// Fetch all
-$result -> fetch_all(MYSQLI_ASSOC);
-
-// Free result set
-$result -> free_result();
-
-$mysqli -> close();
-echo "Query success";
-?>
